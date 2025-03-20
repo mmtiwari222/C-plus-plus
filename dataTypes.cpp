bb91a5bfde54078ad847971dca6fa66662a890ed
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-03-20 22:17:02 +0530

    Basic Data Types in C++ Completed

---
+++ dataTypes.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    // C++ Data Types
+    // data types are used to define the type of data a variable can hold.
+    // C++ has several built-in data types, including:
+    
+    // 1. Integer - A whole number without a decimal point
+    int a = 10;
+    cout << "Integer: " << a << endl;
+
+    // 2. Float - A number with a decimal point
+    float b = 5.5;
+    cout << "Float: " << b << endl;
+
+    // 3. Double - A double-precision floating-point number
+    double c = 10.99;
+    cout << "Double: " << c << endl;
+
+    // 4. Character - A single character
+    char d = 'A';
+    cout << "Character: " << d << endl;
+
+    // 5. Boolean - A true or false value
+    // Note: In C++, true is represented as 1 and false as 0
+    bool e = true;
+    cout << "Boolean: " << e << endl;
+
+    // 6. String - A sequence of characters
+    string f = "Hello, World!";
+    cout << "String: " << f << endl;
+
+}
