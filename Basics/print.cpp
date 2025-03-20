c3fdc18a5558ec82aa6d9c4fe55668c5fbf18eaf
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-03-20 21:53:10 +0530

    C++ Intro & Boilerplate Code

---
+++ Basics/print.cpp
@@ -0,0 +1,20 @@
+// programming language - it is a set of rules and syntax to write a program in a particular language
+// compiler - it is a program that takes a program written in a programming language and converts it into a format that can be executed by the computer (in our case a binary file)
+// interpreter - it is a program that takes a program written in a programming language and executes it line by line
+// low-level-language - it is a programming language that is closer to machine language than human language
+// c++ - it is a programming language, it is a compiled language, it follows the object-oriented programming paradigm, it is a general-purpose programming language
+// high-level-language - it is a programming language that is easy to read and write, it is closer to human language than machine language
+
+
+
+// c++ boilerplate
+#include <iostream> // iostream - it is a header file, it is used to perform input and output operations
+using namespace std; // std - it is a namespace, it is used to define standard names 
+// namespace - it is a declarative region that provides a scope to the identifiers (the names of types, functions, variables, etc.) inside it
+int main() // main function - it is the entry point of a c++ program
+{ // scope of function
+    cout << "Hello World\n"; // cout - it is an object of class iostream, it is used to print output to the console
+    // << - it is an operator, it is used to send output to the console
+    // ; - it is a statement terminator, it is used to mark the end of a statement
+    // endl (escape sequence) (\n) - it is a manipulator, it is used to insert a new line and flush the output buffer
+}
