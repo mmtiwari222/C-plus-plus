85a34b677a472a0a8a2b0d19259aa67020e736bf
Madanmohan Tiwari <mmtiwari222@gmail.com>
2025-03-21 22:45:44 +0530

    Taking input from user in C++

---
+++ Basics/input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+ using namespace std;
+
+ int main(){
+     int a;
+     cout<<"Enter a number: ";
+     cin >> a; 
+     // cin - it is an object of class iostream, it is used to read input from the console
+     cout << "You entered: " << a << endl;
+ }
